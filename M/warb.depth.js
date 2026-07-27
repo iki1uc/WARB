5f4dd0d0caaa69392b4fe39b2beba0c1853d8392
iki1uc <entertrainer@web.de>
2026-07-27 06:52:58 +0200

    Update warb.depth.js

--- M/warb.depth.js
+++ M/warb.depth.js
@@ -1,1 +1,9 @@
+export function warbColor(T){
+    if(T === "T6") return "green";
+    if(T === "T5") return "lime";
+    if(T === "T4") return "yellow";
+    if(T === "T3") return "orange";
+    if(T === "T2") return "red";
+    return "darkred"; // T1
+}
 
